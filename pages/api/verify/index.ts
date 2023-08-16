cb01babdd6f0f4e794a5d324f8356ff9278ac424
kurodenjiro <kurodenjiro1@gmail.com>
2023-08-16 15:50:36 +0700

    update

--- pages/api/verify/index.ts
+++ pages/api/verify/index.ts
@@ -1,12 +1,17 @@
 import { NextApiResponse, NextApiRequest } from 'next'
 import type { Contract, ResponseError , Verify } from '../../../interfaces'
 import fs from 'fs'
+import path from 'path'
 import toml from 'toml';
+
+let codeSource = [];
+
 export default function handler(
   _req: NextApiRequest,
   res: NextApiResponse<Verify | ResponseError>
 ) {
 	let abi = {};
+	
 	const contractId = _req.query.contractId;
 	const pathSource = `./contract/${contractId}/verify.json`
 	const cargoSource = `./contract/${contractId}/Cargo.toml`
@@ -18,10 +23,29 @@ export default function handler(
 	}
 
 	if (fs.existsSync(pathSource)) {
+		traverseDir(`./contract/${contractId}/src`);
+		const sourceView = []
+		console.log(codeSource);
+		codeSource.forEach(item =>{
+			sourceView.push({path:item.replace(`contract/${contractId}/`,""),code:JSON.stringify(fs.readFileSync(item, 'utf-8'))})
+		})
+
 		const verify = JSON.parse(fs.readFileSync(pathSource, 'utf-8'));
-		return res.status(200).json({status:verify.status,abi:JSON.parse(abi)})
+		return res.status(200).json({status:verify.status,abi:JSON.parse(abi),sourceView:sourceView})
 	}else{
 		return res.status(200).json({status:false})
 	}
   
+}
+function traverseDir(dir) {
+	fs.readdirSync(dir).forEach(file => {
+		let fullPath = path.join(dir, file);
+		if (fs.lstatSync(fullPath).isDirectory()) {
+			 //console.log(fullPath);
+			 traverseDir(fullPath);
+		 } else {
+			 //\console.log(fullPath);
+			 codeSource.push(fullPath);
+		 }  
+	});
 }
